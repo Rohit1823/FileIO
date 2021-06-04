1396b679a25d9e41dab5cd442b4577ef01bea327
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-04 12:04:55 +0530

    [Rohit]ADD: FileIO Project File

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileExists();
+            //ReadAllLines();
+            //ReadAllText();
+            //FileCopy();
+            //DeleteFile();
+            ReadFromStreamReader();
+            Console.ReadKey();
+        }
+
+        public static void FileExists()
+        {
+            String path = @"C:\Users\91708\source\repos\FileIO\FileIO\Example.txt";
+            if (File.Exists(path))
+                Console.WriteLine("File exists");
+            else
+                Console.WriteLine("File doesn't exists");
+        }
+
+        public static void ReadAllLines()
+        {
+            String path = "C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt";
+            String[] lines;
+            //C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+        }
+
+        public static void ReadAllText()
+        {
+            String path = "C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt";
+            String lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+        }
+
+        public static void FileCopy()
+        {
+            string path = "C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt";
+            string copypath = "C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt";
+
+            File.Copy(path, copypath);
+
+        }
+
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\91708\source\repos\FileIO\FileIO\Example.txt";
+
+            File.Delete(path);
+
+        }
+
+        public static void ReadFromStreamReader()
+        {
+            String path = "C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+
+        public static void WriteUsingStreamWriter()
+        {
+            String path = "C:\\Users\\91708\\source\\repos\\FileIO\\FileIO\\Example.txt";
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hellow World - .Net is awesome");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+
+
+    }
+}
---
+++ FileIO/SerializationDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SerializationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySerialization obj = new BinarySerialization();
+            // obj.Serialization();
+            BinaryDeSerialization objDesrialize = new BinaryDeSerialization();
+            objDesrialize.DeSerialization();
+        }
+    }
+}
